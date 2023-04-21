cc9d27a4f117a2db55ad41ccbf7b722556823985
claudios883 <claudioalejandro4@gmail.com>
2023-04-21 14:34:40 -0600

    DB connection

--- src/main/java/com/example/securitysystemcoreservice/Controllers/GreetingController.java
+++ src/main/java/com/example/securitysystemcoreservice/Controllers/GreetingController.java
@@ -1,4 +1,5 @@
 package com.example.securitysystemcoreservice.Controllers;
+import  com.example.securitysystemcoreservice.Models.Greeting;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -8,12 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
-
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
-
     @GetMapping("/greeting")
-    public com.example.securitysystemcoreservice.Models.Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new com.example.securitysystemcoreservice.Models.Greeting(counter.incrementAndGet(), String.format(template, name));
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return Greeting.makeCompleteGreeting(name);
     }
 }
--- src/main/java/com/example/securitysystemcoreservice/Models/Greeting.java
+++ src/main/java/com/example/securitysystemcoreservice/Models/Greeting.java
@@ -1,5 +1,25 @@
 package com.example.securitysystemcoreservice.Models;
 
-public record Greeting (long id,String content){
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Greeting {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    String content;
+
+    public Greeting(){
+        this.content = "Hello %s";
+    }
+
+    public static Greeting makeCompleteGreeting(String name){
+        Greeting greeting = new Greeting();
+        greeting.content = String.format(greeting.content, name);
+        return greeting;
+    }
 
 }
